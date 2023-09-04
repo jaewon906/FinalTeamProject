4bee7d783dacfa01cf52a459d4ac1ff68622b2a5
juca <dhzlehzl01@naver.com>
2023-09-04 11:08:58 +0900

    장바구니 페이지 디자인 수정
    장바구니 페이지 구매/삭제 위치 수정 및 구매하기 누를 시 구매 페이지로 이동 처리 중

--- src/main/frontend/src/pages/CartPage/index.js
+++ src/main/frontend/src/pages/CartPage/index.js
@@ -8,13 +8,11 @@ import Button from "../../component/common/Button";
 function CartPage() {
   const [cart, setCart] = useState({ cartItems: [] });
   const [selectedItems, setSelectedItems] = useState([]);
-  const [quantity, setQuantity] = useState(1);
 
-  // 장바구니 상품 수량 조절
- 
-  
+  // 장바구니 상품 가격
+
   // 사용자의 userNumber를 가져오기
-   const userNumber = getUserNumber().userNumber;
+  const userNumber = getUserNumber().userNumber;
 
   useEffect(() => {
     // 백엔드에서 장바구니 정보를 가져와서 상태 업데이트
@@ -26,6 +24,7 @@ function CartPage() {
       .get(`/api/cart/${userNumber}`)
       .then((response) => {
         // 가져온 장바구니 정보를 상태에 설정
+        console.log("response data " + response.data);
         setCart(response.data);
         console.log(response.data);
         fetchBookInfo(response.data.cartItems);
@@ -43,11 +42,13 @@ function CartPage() {
         console.log("Response Data", response.data);
         // 도서 정보를 가져온 뒤, cartItems에 병합하여 상태 업데이트
         const updatedCartItems = cartItems.map((item) => {
-          const bookInfo = response.data.find((book) => 
-            book.bookId === item.bookId
+          const bookInfo = response.data.find(
+            (book) => book.bookId === item.bookId
           );
-          if(bookInfo) {
-            const imgUrlArray = JSON.parse(bookInfo.previewImgList.replace(/\\/g, ""));
+          if (bookInfo) {
+            const imgUrlArray = JSON.parse(
+              bookInfo.previewImgList.replace(/\\/g, "")
+            );
             return {
               ...item,
               bookInfo: {
@@ -59,10 +60,10 @@ function CartPage() {
             return {
               ...item,
               bookInfo: { previewImgList: [] }, // 빈 배열로 초기값 설정
-            }
+            };
           }
         });
-        setCart((prevCart) => ({...prevCart, cartItems: updatedCartItems }));
+        setCart((prevCart) => ({ ...prevCart, cartItems: updatedCartItems }));
       })
       .catch((error) => {
         console.log(error);
@@ -70,7 +71,7 @@ function CartPage() {
   };
 
   const handleCheckBoxChange = (itemId) => {
-    if(selectedItems.includes(itemId)) {
+    if (selectedItems.includes(itemId)) {
       setSelectedItems(selectedItems.filter((id) => id !== itemId));
     } else {
       setSelectedItems([...selectedItems, itemId]);
@@ -80,26 +81,29 @@ function CartPage() {
   const handleDeleteItems = () => {
     const ret = window.confirm("상품을 삭제하겠습니까?");
 
-    if(ret) {
-    axios
-      .delete(`/api/cart/${userNumber}/items`, {
-        data: selectedItems
-      })
-      .then((response) => {
-        // 백엔드에서 삭제가 성공하면 프론트에서도 상품 제거
-        setCart((prevCart) => ({
-          ...prevCart,
-          cartItems: prevCart.cartItems.filter(
-            (item) => !selectedItems.some(selectedId => selectedId === item.bookInfo.bookId)
-          ),
-        }));
-        setSelectedItems([]); // 선택된 아이템 초기화
-        // 삭제 후 장바구니 정보 다시 업데이트
-        fetchCartItems(userNumber);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (ret) {
+      axios
+        .delete(`/api/cart/${userNumber}/items`, {
+          data: selectedItems,
+        })
+        .then((response) => {
+          // 백엔드에서 삭제가 성공하면 프론트에서도 상품 제거
+          setCart((prevCart) => ({
+            ...prevCart,
+            cartItems: prevCart.cartItems.filter(
+              (item) =>
+                !selectedItems.some(
+                  (selectedId) => selectedId === item.bookInfo.bookId
+                )
+            ),
+          }));
+          setSelectedItems([]); // 선택된 아이템 초기화
+          // 삭제 후 장바구니 정보 다시 업데이트
+          fetchCartItems(userNumber);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } else {
       return;
     }
@@ -113,56 +117,104 @@ function CartPage() {
             <caption>장바구니</caption>
             <tbody>
               {cart.cartItems.map((item) => {
-                return(
+                const quantity = item.quantity;
+                const priceSales = item.bookInfo
+                  ? parseInt(item.bookInfo.priceSales)
+                  : 0;
+                const priceStd = item.bookInfo
+                  ? parseInt(item.bookInfo.priceStandard)
+                  : 0;
+                const totalPrice = item.bookInfo
+                  ? item.bookInfo.priceSales * quantity
+                  : 0;
+
+                const formattedPriceSales = priceSales.toLocaleString();
+                const formattedPriceStd = priceStd.toLocaleString();
+                const formattedTotalPrice = totalPrice.toLocaleString();
+
+                return (
                   <tr key={item.id}>
-                  <td className={styles.checkbox}>
-                    <input 
-                      type="checkbox"
-                      checked={selectedItems.includes(item.id)}
-                      onChange={() => handleCheckBoxChange(item.id)}
+                    <td className={styles.checkbox}>
+                      <input
+                        type="checkbox"
+                        checked={selectedItems.includes(item.id)}
+                        onChange={() => handleCheckBoxChange(item.id)}
                       />
-                  </td>
-                  <td className="item">
-                    <div className={styles.itemArea}>
-                      <div className="cover-box">
+                    </td>
+                    <td className="item">
+                      <div className={styles.itemArea}>
+                        <div className="cover-box">
                           {item.bookInfo ? (
-                            <Link to={`/home/bookdetail/${item.bookInfo.isbn13}/`}>
-                            <img src={item.bookInfo.previewImgList[0]} alt="book" width="100px" height="150px" />
+                            <Link
+                              to={`/home/bookdetail/${item.bookInfo.isbn13}/`}
+                            >
+                              <img
+                                src={item.bookInfo.previewImgList[0]}
+                                alt="book"
+                                width="100px"
+                                height="150px"
+                              />
                             </Link>
                           ) : (
                             <div>no image</div>
                           )}
-                      </div>
-                      <div className={styles.itemInfoBox}>
-                        {item.bookInfo ? (
-                          <Link to={`/home/bookdetail/${item.bookInfo.isbn13}/`}>{item.bookInfo.title}</Link>
-                        ) : (
-                          <div>Loading...</div>
-                        )}
+                        </div>
+                        <div className={styles.itemInfoBox}>
+                          {item.bookInfo ? (
+                            <Link
+                              to={`/home/bookdetail/${item.bookInfo.isbn13}/`}
+                            >
+                              {item.bookInfo.title}
+                            </Link>
+                          ) : (
+                            <div>Loading...</div>
+                          )}
 
-                        {item.bookInfo ? (
-                          <div className="item-price">
-                            <span className={styles.green}>10%</span>{item.bookInfo.priceSales}원 <del className={styles.del}>{item.bookInfo.priceStandard}원</del>
-                            <p>{item.bookInfo.author} · {item.bookInfo.publisher}</p>
-                          </div>
-                        ) : (
-                          <div>Loading...</div>
-                        )}
-
+                          {item.bookInfo ? (
+                            <div className="item-price">
+                              <span className={styles.green}>10%</span>
+                              {formattedPriceSales}원
+                              <del className={styles.del}>
+                                {formattedPriceStd}원
+                              </del>
+                              <p>
+                                {item.bookInfo.author} ·{" "}
+                                {item.bookInfo.publisher}
+                              </p>
+                            </div>
+                          ) : (
+                            <div>Loading...</div>
+                          )}
+                        </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>
-                  
-                  </td>
-                  <td>
-                    <Button red="true" onClick={handleDeleteItems}>삭제</Button>
-                  </td>
-                </tr>
-                )
+                    </td>
+                    <td className={styles.center}>
+                      {item.bookInfo ? (
+                        <div>{formattedTotalPrice}원</div>
+                      ) : (
+                        <div>Loading...</div>
+                      )}
+                    </td>
+                    <td>
+                      <div className={styles.center}>배송료 : 무료</div>
+                    </td>
+                  </tr>
+                );
               })}
             </tbody>
           </table>
+        </div>
+        <div className={styles.btnBox}>
+          <div className={styles.btn}>
+            <Button green="true" fullWidth>
+              구매하기
+            </Button>
+          </div>
+          <div className={styles.btn}>
+            <Button red="true" fullWidth onClick={handleDeleteItems}>
+              삭제
+            </Button>
+          </div>
         </div>
       </div>
     </div>
