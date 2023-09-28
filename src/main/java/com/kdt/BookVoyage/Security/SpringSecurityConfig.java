50e749d026a22a26fc31895613510ee26ab2e603
Park Jae Won <ploi9@naver.com>
2023-09-28 23:51:37 +0900

    "CAUSE : malicious string '//' security`s strictHttpFirewall -> DefaultHttpFirewall"

---
+++ src/main/java/com/kdt/BookVoyage/Security/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.kdt.BookVoyage.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer configure(){
+        return (webSecurity)->{webSecurity.httpFirewall(defaultHttpFirewall());};
+    }
+
+    @Bean
+    public HttpFirewall defaultHttpFirewall(){
+        return new DefaultHttpFirewall();
+    }
+}
